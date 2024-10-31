4e2904b33a393a9d6263492e8146122fcfe91f11
Varvara <varya.shevaldova@mail.ru>
2024-10-31 16:14:19 +0400

    lesson3-update2

--- dev.py
+++ dev.py
@@ -1,15 +1,31 @@
-import json, csv
+import json
+import csv
+import types
 
-result = None
 
-with (open('in.json', 'r') as file):
-    arr = json.load(file)
-    result = ([i['name'], i['address'].replace('\n', ' '), i['email']] for i in arr if
-              (i['phoneNumber'].startswith('+1') or i['phoneNumber'].startswith('1')) and '4.0 Safari' in i[
-                  "userAgent"])
+def filter_fields(dt: dict) -> bool:
+    if (dt['phoneNumber'].startswith('+1') or dt['phoneNumber'].startswith('1')) and '4.0 Safari' in dt["userAgent"]:
+        return True
+    else:
+        return False
 
-with open('result.csv', 'w') as file:
-    writer = csv.writer(file)
-    writer.writerow(['name', 'address', 'mail'])
-    for j in result:
-        writer.writerow(j)
+
+def read_json(read_file: str) -> types.GeneratorType:
+    with (open(read_file, 'r') as file):
+        arr: list = json.load(file)
+        for i in arr:
+            if filter_fields(i):
+                yield [i['name'], i['address'].replace('\n', ' '), i['email']]
+
+
+def write_csv(write_file: str, gener: types.GeneratorType) -> None:
+    with open(write_file, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(['name', 'address', 'mail'])
+        for j in gener:
+            writer.writerow(j)
+
+
+result_generator = read_json('in.json')
+write_csv('result.csv', result_generator)
+
